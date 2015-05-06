c07cc35573606fd5ae5bcb4abac2e155d2e91161
Vignesh-Shanmugam <s.vig14@gmail.com>
2015-05-06 23:25:11 +0530

    Update EasEmailNotificationApplication.java

--- src/main/java/com/EasEmailNotificationApplication.java
+++ src/main/java/com/EasEmailNotificationApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasEmailNotificationApplication {
+public class EmailNotificationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EasEmailNotificationApplication.class, args);
+        SpringApplication.run(EmailNotificationApplication.class, args);
     }
 }
